13835a78164d35a8c72e70f85cc4494433407218
COSTA Dev <guilhermeoliveira.tech@outlook.com.br ]>
2026-01-28 08:42:16 -0300

    Adicionado a configuração do Cors

---
+++ src/main/java/com/costadev/bff_agendadortarefas/infrastructure/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.costadev.bff_agendadortarefas.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    public WebMvcConfigurer configCors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://localhost:4200") // porta do front-end
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(360);
+            }
+        };
+    }
+}
