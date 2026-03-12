3f9a921ffdfc397ce042528d6b129be237b42c33
guilhermeoliveira-software <guilhermeoliveira.tech@outlook.com.br>
2026-03-12 13:34:00 -0300

    Validando correção dos Code Smells

--- src/main/java/com/costadev/bff_agendadortarefas/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/costadev/bff_agendadortarefas/infrastructure/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SecurityScheme(name = SecurityConfig.SECURITY_SCHEME, type = SecuritySchemeType.HTTP,
                 bearerFormat = "JWT", scheme = "bearer")
 public class SecurityConfig {
+
         public static final String SECURITY_SCHEME = "bearerAuth";
 
 }
